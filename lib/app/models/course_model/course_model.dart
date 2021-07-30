7b7a1046f0438700b205059335d4f6f09c94e82b
Instantki <alvarob.prado@hotmail.com>
2021-07-30 17:57:47 -0400

    fix: changed course model props to nullable

--- lib/app/models/course_model/course_model.dart
+++ lib/app/models/course_model/course_model.dart
@@ -1,10 +1,10 @@
 import 'package:get/get.dart';
 
 class CourseModel {
-  RxString name;
-  RxString description;
-  CourseModel({
-    required this.name,
-    required this.description,
-  });
+  RxString? name;
+  RxString? description;
+  // CourseModel({
+  //   required this.name,
+  //   required this.description,
+  // });
 }
