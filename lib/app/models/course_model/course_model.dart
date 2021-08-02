849329bc7e1938bf92f821be1e0b546e70ef0c13
Instantki <alvarob.prado@hotmail.com>
2021-08-02 18:36:38 -0400

    feat: added course model constructor

--- lib/app/models/course_model/course_model.dart
+++ lib/app/models/course_model/course_model.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CourseModel {
-  Rx<String>? name;
-  Rx<String>? description;
-  Rx<Image> logoImage = Image.asset("assets/imgs/course-empty.png").obs;
+  Rx<String> name;
+  Rx<String> description;
+  Rx<Image> logoImage;
   CourseModel({
-    this.name,
-    this.description,
-  });
+    required this.name,
+    required this.description,
+    Image? image,
+  }) : logoImage =
+            image?.obs ?? Image.asset("assets/imgs/course-empty.png").obs;
 }
