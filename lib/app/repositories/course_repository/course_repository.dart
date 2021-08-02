9f977fec43a9d721f221c0f7f5a36a973bdf30da
Instantki <alvarob.prado@hotmail.com>
2021-08-02 18:36:55 -0400

    feat: added course repository methods

--- lib/app/repositories/course_repository/course_repository.dart
+++ lib/app/repositories/course_repository/course_repository.dart
@@ -1,9 +1,46 @@
 import 'package:dio_cursos/app/models/course_model/course_model.dart';
+import 'package:get/get.dart';
 
 class CourseRepository {
-  void save(CourseModel curse) {}
+  final courses = <CourseModel>[
+    CourseModel(
+      name: "Teste Curso 1".obs,
+      description: "Teste descrição".obs,
+    ),
+    CourseModel(
+      name: "Teste Curso 2".obs,
+      description: "Teste descrição".obs,
+    ),
+    CourseModel(
+      name: "Teste Curso 3".obs,
+      description: "Teste descrição".obs,
+    ),
+    CourseModel(
+      name: "Teste Curso 4".obs,
+      description: "Teste descrição".obs,
+    ),
+    CourseModel(
+      name: "Teste Curso 5".obs,
+      description: "Teste descrição".obs,
+    ),
+    CourseModel(
+      name: "Teste Curso 6".obs,
+      description: "Teste descrição".obs,
+    )
+  ];
+
+  void save(CourseModel course) {
+    if (courses.contains(course)) {
+      courses.remove(course);
+    }
+    courses.add(course);
+  }
+
+  void remove(CourseModel course) {
+    courses.remove(course);
+  }
 
   List<CourseModel> findAllCourses() {
-    return [];
+    return courses;
   }
 }
