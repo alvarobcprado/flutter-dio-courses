cc1703ee3cbdc3823581f26976399ec577a0ba0e
Instantki <alvarob.prado@hotmail.com>
2021-08-02 19:12:34 -0400

    fix: changed course list view to observable

--- lib/app/views/home_view/widgets/courses_list_view.dart
+++ lib/app/views/home_view/widgets/courses_list_view.dart
@@ -5,66 +5,58 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 
 class CoursesListView extends StatelessWidget {
-  final CourseRepository courseRepo = CourseRepository();
+  final CourseRepository courseRepo = Get.put(CourseRepository());
 
   @override
   Widget build(BuildContext context) {
-    var courseList = [
-      CourseModel(
-        name: "Teste Curso".obs,
-        description: "Teste descrição".obs,
-      ),
-      CourseModel(
-        name: "Teste Curso".obs,
-        description: "Teste descrição".obs,
-      ),
-      CourseModel(
-        name: "Teste Curso".obs,
-        description: "Teste descrição".obs,
-      )
-    ];
-    return ListView.separated(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemBuilder: (builder, index) {
-        CourseModel course = courseList[index];
-        return Slidable(
-          direction: Axis.horizontal,
-          actionPane: SlidableDrawerActionPane(),
-          actionExtentRatio: 0.25,
-          child: ListTile(
-            leading: CircleAvatar(
-              child: course.logoImage.value,
-            ),
-            title: Text(
-              course.name!.value,
-              style: TextStyle(color: Colors.white70),
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          secondaryActions: [
-            IconSlideAction(
-              caption: "Editar",
-              color: Colors.black45,
-              icon: Icons.edit,
-              onTap: () => _navigateToEdit(course),
-            ),
-            IconSlideAction(
-              caption: "Deletar",
-              color: Colors.red,
-              icon: Icons.delete,
-              onTap: () => _deleteCourse(course),
-            ),
-          ],
-        );
-      },
-      separatorBuilder: (context, index) => Divider(),
-      itemCount: courseList.length,
-    );
+    var courseList = courseRepo.findAllCourses();
+
+    return Obx(() => ListView.separated(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemBuilder: (builder, index) {
+            var course = courseList[index];
+            return Slidable(
+              direction: Axis.horizontal,
+              actionPane: SlidableDrawerActionPane(),
+              actionExtentRatio: 0.25,
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: course.logoImage.value,
+                ),
+                title: Text(
+                  course.name.value,
+                  style: TextStyle(color: Colors.white70),
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              secondaryActions: [
+                IconSlideAction(
+                  caption: "Editar",
+                  color: Colors.black45,
+                  icon: Icons.edit,
+                  onTap: () => _navigateToEdit(course),
+                ),
+                IconSlideAction(
+                  caption: "Deletar",
+                  color: Colors.red,
+                  icon: Icons.delete,
+                  onTap: () => _deleteCourse(course),
+                ),
+              ],
+            );
+          },
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: courseList.length,
+        ));
   }
 
-  _navigateToEdit(CourseModel course) {}
+  _navigateToEdit(CourseModel course) {
+    Get.toNamed("/courses", arguments: course);
+  }
 
-  _deleteCourse(CourseModel course) {}
+  _deleteCourse(CourseModel course) {
+    courseRepo.remove(course);
+  }
 }
