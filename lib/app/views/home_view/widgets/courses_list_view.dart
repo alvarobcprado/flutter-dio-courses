0040bc6cf76e52169512158604d713f7a7656dcc
Instantki <alvarob.prado@hotmail.com>
2021-08-09 10:37:10 -0400

    feat: added course activity progress bar

--- lib/app/views/home_view/widgets/courses_list_view.dart
+++ lib/app/views/home_view/widgets/courses_list_view.dart
@@ -1,10 +1,15 @@
+import 'dart:ui';
+
+import 'package:dio_cursos/app/controllers/activity_controller/activity_controller.dart';
 import 'package:dio_cursos/app/controllers/course_controller/course_controller.dart';
+import 'package:dio_cursos/app/models/course_model/course_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 
 class CoursesListView extends StatelessWidget {
   final courseCtrl = Get.find<CourseController>();
+  final activityCtrl = Get.put<ActivityController>(ActivityController());
 
   @override
   Widget build(BuildContext context) {
@@ -19,22 +24,7 @@ class CoursesListView extends StatelessWidget {
               direction: Axis.horizontal,
               actionPane: SlidableDrawerActionPane(),
               actionExtentRatio: 0.25,
-              child: ListTile(
-                leading: Obx(
-                  () => CircleAvatar(
-                    radius: 30,
-                    backgroundImage: course.logoImage.value.image,
-                    backgroundColor: Colors.transparent,
-                  ),
-                ),
-                title: Obx(() => Text(
-                      course.name.value,
-                      style: TextStyle(color: Colors.white),
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                    )),
-                onTap: () => Get.toNamed("activities", arguments: course),
-              ),
+              child: _buildCourseListTile(course),
               secondaryActions: [
                 IconSlideAction(
                   caption: "Editar",
@@ -54,5 +44,76 @@ class CoursesListView extends StatelessWidget {
           separatorBuilder: (context, index) => Divider(),
           itemCount: courseCtrl.courseList.length,
         ));
+  }
+
+  Widget _buildCourseListTile(CourseModel course) {
+    var activities = activityCtrl.findAllActivitiesByCourse(course);
+    var completedActivities =
+        activities.where((e) => e.checked.value == true).length;
+    return ListTile(
+      leading: Obx(
+        () => CircleAvatar(
+          radius: 30,
+          backgroundImage: course.logoImage.value.image,
+          backgroundColor: Colors.transparent,
+        ),
+      ),
+      title: Obx(
+        () => Text(
+          course.name.value,
+          style: TextStyle(color: Colors.white),
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Obx(
+            () => Text(
+              course.description.value,
+              style: TextStyle(color: Colors.white70),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          activities.length > 0
+              ? Obx(
+                  () => _progressCourseBar(
+                    percentCompleted: completedActivities / activities.length,
+                  ),
+                )
+              : Container(),
+        ],
+      ),
+      onTap: () => Get.toNamed("activities", arguments: course),
+    );
+  }
+
+  Widget _progressCourseBar({required double percentCompleted}) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            height: 10,
+            child: ClipRRect(
+              child: LinearProgressIndicator(
+                value: percentCompleted,
+                backgroundColor: Colors.grey.withAlpha(50),
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            (percentCompleted * 100).round().toString() + "%",
+            style: TextStyle(color: Colors.white),
+          ),
+        )
+      ],
+    );
   }
 }
